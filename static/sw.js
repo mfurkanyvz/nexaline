0dd5f655ea882298ee5a875792fe7faea4eae75b
mfurkanyvz <mfurkanyvz@gmail.com>
2026-06-14 02:08:31 +0300

    Make voice transcripts user initiated

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nexaline-pwa-v67-attachment-icons-20260614";
+const CACHE_NAME = "nexaline-pwa-v68-voice-transcript-20260614";
 const APP_SHELL = [
   "/manifest.webmanifest",
   "/static/vendor/socket.io.min.js",
