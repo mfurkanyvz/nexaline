ef42dd7b603b909693bcfb1c018f6318e03a71ca
mfurkanyvz <mfurkanyvz@gmail.com>
2026-06-13 15:07:50 +0300

    Stabilize glass bottom navigation

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nexaline-pwa-v57-stage8-admin";
+const CACHE_NAME = "nexaline-pwa-v58-stage21-nav";
 const APP_SHELL = [
   "/manifest.webmanifest",
   "/static/vendor/socket.io.min.js",
