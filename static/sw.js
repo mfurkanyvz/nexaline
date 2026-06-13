2b954c39949a2273bd852a34132c2809c9c23e9a
mfurkanyvz <mfurkanyvz@gmail.com>
2026-06-14 01:41:14 +0300

    Stabilize PWA startup cache refresh

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nexaline-pwa-v63-logo-20260614";
+const CACHE_NAME = "nexaline-pwa-v64-boot-20260614";
 const APP_SHELL = [
   "/manifest.webmanifest",
   "/static/vendor/socket.io.min.js",
