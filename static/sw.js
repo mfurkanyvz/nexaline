ef316b59f9ddd4b78dc0fc54f384e936b6cce454
mfurkanyvz <mfurkanyvz@gmail.com>
2026-06-14 22:53:23 +0300

    Fix profile photo cropper layout

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nexaline-pwa-v76-white-mode-labels-20260614";
+const CACHE_NAME = "nexaline-pwa-v77-profile-cropper-20260614";
 const APP_SHELL = [
   "/manifest.webmanifest",
   "/static/vendor/socket.io.min.js",
